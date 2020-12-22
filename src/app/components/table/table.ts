fc7134c4010aa5ffc38b6d0c72ca9262f4faa0f6
Viktor Nekuryashchikh <viktorn@newspaperdirect.com>
2020-12-22 13:56:36 -0800

    Fixed scrolling

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1247,7 +1247,7 @@ export class Table implements OnInit, AfterViewInit, AfterContentInit, Blockable
         }
 
         this.filterTimeout = setTimeout(() => {
-            this._filter();
+            this._filter(true);
             this.filterTimeout = null;
         }, this.filterDelay);
 
@@ -1268,7 +1268,7 @@ export class Table implements OnInit, AfterViewInit, AfterContentInit, Blockable
         return true;
     }
 
-    _filter() {
+    _filter(resetScroll) {
         if (!this.restoringFilter) {
             this.first = 0;
             this.firstChange.emit(this.first);
@@ -1375,7 +1375,8 @@ export class Table implements OnInit, AfterViewInit, AfterContentInit, Blockable
 
         this.cd.markForCheck();
 
-        if (this.scrollable) {
+        if (this.scrollable && resetScroll) {
+            // reset scrolling only when this call is initiated by filter event
             this.resetScrollTop();
         }
     }
